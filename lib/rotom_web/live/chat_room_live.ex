fcfbf10302adfae4aa1d5f6a6b53b235b8ad0a53
charmingruby <mrrubycanal3105@gmail.com>
2025-07-14 19:18:19 -0300

    feat: displays room data

--- lib/rotom_web/live/chat_room_live.ex
+++ lib/rotom_web/live/chat_room_live.ex
@@ -1,11 +1,31 @@
 defmodule RotomWeb.ChatRoomLive do
+  alias Rotom.Chat.Room
+  alias Rotom.Repo
   use RotomWeb, :live_view
 
   def render(assigns) do
     ~H"""
-    <div>
-      <h1>Welcome</h1>
+    <div class="flex flex-col grow shadow-lg">
+      <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-sm font-bold leading-none">
+            # {@room.name}
+          </h1>
+          <div class="text-xs leading-none h-3.5">
+            {@room.topic}
+          </div>
+        </div>
+      </div>
     </div>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    room =
+      Room
+      |> Repo.all()
+      |> List.first()
+
+    {:ok, assign(socket, :room, room)}
   end
 end
