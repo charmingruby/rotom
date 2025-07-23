36b7f5e3d5f6002812702414c0662f04dac7bf0d
charmingruby <mrrubycanal3105@gmail.com>
2025-07-23 16:26:15 -0300

    feat: adds data toggle on sidebar

--- lib/rotom_web/live/chat_room_live.ex
+++ lib/rotom_web/live/chat_room_live.ex
@@ -18,19 +18,15 @@ defmodule RotomWeb.ChatRoomLive do
         </div>
       </div>
       <div class="mt-4 overflow-auto">
-        <div class="flex items-center h-8 px-3">
-          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
-        </div>
+        <.toggler on_click={toggle_rooms()} dom_id="rooms-toggler" text="Rooms" />
+
         <div id="rooms-list">
           <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
         </div>
 
-        <div class="mt-4">
-          <div class="flex items-center h-8 px-3">
-            <div class="flex items-center grow">
-              <span class="ml-2 leading-none font-medium text-sm">Users</span>
-            </div>
-          </div>
+        <div>
+          <.toggler on_click={toggle_users()} dom_id="users-toggler" text="Users" />
+
           <div id="users-list">
             <.user
               :for={user <- @users}
@@ -142,6 +138,29 @@ defmodule RotomWeb.ChatRoomLive do
           </button>
         </.form>
       </div>
+    </div>
+    """
+  end
+
+  attr :dom_id, :string, required: true
+  attr :text, :string, required: true
+  attr :on_click, JS, required: true
+
+  defp toggler(assigns) do
+    ~H"""
+    <div class="flex items-center h-8 px-3">
+      <button id={@dom_id} phx-click={@on_click} class="flex items-center grow">
+        <.icon id={@dom_id <> "-chevron-down"} name="hero-chevron-down" class="h-4 w-4" />
+        <.icon
+          id={@dom_id <> "-chevron-right"}
+          name="hero-chevron-right"
+          class="h-4 w-4"
+          style="display:none;"
+        />
+        <span class="ml-2 leading-none font-medium text-sm">
+          {@text}
+        </span>
+      </button>
     </div>
     """
   end
@@ -318,6 +337,18 @@ defmodule RotomWeb.ChatRoomLive do
 
   defp assign_message_form(socket, changeset) do
     assign(socket, :new_message_form, to_form(changeset))
+  end
+
+  defp toggle_rooms do
+    JS.toggle(to: "#rooms-toggler-chevron-down")
+    |> JS.toggle(to: "#rooms-toggler-chevron-right")
+    |> JS.toggle(to: "#rooms-list")
+  end
+
+  defp toggle_users do
+    JS.toggle(to: "#users-toggler-chevron-down")
+    |> JS.toggle(to: "#users-toggler-chevron-right")
+    |> JS.toggle(to: "#users-list")
   end
 
   defp username(user) do
