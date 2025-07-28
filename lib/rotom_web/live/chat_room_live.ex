f6e090f3d439b40744c99b99b071636f45bd592e
charmingruby <mrrubycanal3105@gmail.com>
2025-07-27 21:44:28 -0300

    feat: create room form

--- lib/rotom_web/live/chat_room_live.ex
+++ lib/rotom_web/live/chat_room_live.ex
@@ -215,7 +215,19 @@ defmodule RotomWeb.ChatRoomLive do
 
     <.modal id="new-room-modal">
       <.header>New chat room</.header>
-      (Form goes here)
+
+      <.simple_form
+        for={@new_room_form}
+        id="room-form"
+        phx-change="validate-room"
+        phx-submit="save-room"
+      >
+        <.input field={@new_room_form[:name]} type="text" label="Name" phx-debounce />
+        <.input field={@new_room_form[:topic]} type="text" label="Topic" phx-debounce />
+        <:actions>
+          <.button phx-disable-with="Saving..." class="w-full">Save</.button>
+        </:actions>
+      </.simple_form>
     </.modal>
     """
   end
@@ -356,6 +368,7 @@ defmodule RotomWeb.ChatRoomLive do
       socket
       |> assign(rooms: rooms, users: users, timezone: timezone)
       |> assign(online_users: OnlineUsers.list())
+      |> assign_room_form(Chat.change_room(%Room{}))
       |> stream_configure(:messages,
         dom_id: fn
           %Message{id: id} -> "messages-#{id}"
@@ -409,6 +422,15 @@ defmodule RotomWeb.ChatRoomLive do
     {:noreply, assign_message_form(socket, changeset)}
   end
 
+  def handle_event("validate-room", %{"room" => room_params}, socket) do
+    changeset =
+      socket.assigns.room
+      |> Chat.change_room(room_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_room_form(socket, changeset)}
+  end
+
   def handle_event("submit-message", %{"message" => message_params}, socket) do
     %{current_user: current_user, room: room} = socket.assigns
 
@@ -432,6 +454,21 @@ defmodule RotomWeb.ChatRoomLive do
     Chat.delete_message_by_id(id, socket.assigns.current_user)
 
     {:noreply, socket}
+  end
+
+  def handle_event("save-room", %{"room" => room_params}, socket) do
+    case Chat.create_room(room_params) do
+      {:ok, room} ->
+        Chat.join_room!(room, socket.assigns.current_user)
+
+        {:noreply,
+         socket
+         |> put_flash(:info, "Created room")
+         |> push_navigate(to: ~p"/rooms/#{room}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_room_form(socket, changeset)}
+    end
   end
 
   def handle_event("join-room", _, socket) do
@@ -490,6 +527,10 @@ defmodule RotomWeb.ChatRoomLive do
 
   defp assign_message_form(socket, changeset) do
     assign(socket, :new_message_form, to_form(changeset))
+  end
+
+  defp assign_room_form(socket, changeset) do
+    assign(socket, :new_room_form, to_form(changeset))
   end
 
   defp maybe_insert_unread_marker(messages, nil), do: messages
